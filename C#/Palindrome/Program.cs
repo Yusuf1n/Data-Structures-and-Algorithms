de292dfd53bf62c1de859e58a1ed96d5eb54f739
Yusuf Naheem <yusufnaheem1@gmail.com>
2021-11-04 10:41:05 +0000

    Update Program.cs

--- C#/Palindrome/Program.cs
+++ C#/Palindrome/Program.cs
@@ -1,16 +1,3 @@
-﻿// Daily Byte
-
-/*
-This question is asked by Facebook. Given a string, return whether or not it forms a palindrome ignoring case and non-alphabetical characters.
-Note: a palindrome is a sequence of characters that reads the same forwards and backwards.
-
-Ex: Given the following strings...
-
-"level", return true
-"algorithm", return false
-"A man, a plan, a canal: Panama.", return true
-*/
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
