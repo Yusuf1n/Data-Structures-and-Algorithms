a3cf450f00fdc5e4822d645edf13cf73da7728c4
Yusuf Naheem <yusufnaheem1@gmail.com>
2021-11-04 10:41:53 +0000

    Update Program.cs

--- C#/Reverse a string/Program.cs
+++ C#/Reverse a string/Program.cs
@@ -1,15 +1,3 @@
-﻿// Daily Byte
-
-/*
-This question is asked by Google. Given a string, reverse all of its characters and return the resulting string.
-
-Ex: Given the following strings...
-
-“Cat”, return “taC”
-“The Daily Byte”, return "etyB yliaD ehT”
-“civic”, return “civic”
-*/
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
